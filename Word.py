b05de9375d20a5bfba3d7a8486fefafb09f9f85d
IMRAN BAALI <imran.baali19@gmail.com>
2020-07-27 17:46:13 +0100

    Word Problem

--- Word.py
+++ Word.py
@@ -1,9 +1,9 @@
 from sys import stdin, stdout
 word = stdin.readline()
- 
+
 upper = sum(1 for i in word if i.isupper())
 lower = sum(1 for i in word if i.islower())
- 
+
 if lower >= upper:
 	stdout.write(word.lower() + "\n")
 else:
