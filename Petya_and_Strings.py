375e4d584db402562ea8b21e8b242a5e617cf42e
IMRAN BAALI <imran.baali19@gmail.com>
2020-07-27 17:43:28 +0100

    Petya and Strings Problem

--- Petya_and_Strings.py
+++ Petya_and_Strings.py
@@ -1,8 +1,8 @@
 from sys import stdin, stdout
- 
+
 string_one = stdin.readline().lower()
 string_two = stdin.readline().lower()
- 
+
 if string_one == string_two:
 	stdout.write("0\n")
 elif string_one > string_two:
