ee4c376dc8dc42b587e33bbc833a887119593dc7
Imran Baali <ibaali@e2r1p8.1337.ma>
2020-12-01 11:38:38 +0100

    add writing code to generate_name file

--- generate_name.py
+++ generate_name.py
@@ -2,4 +2,20 @@ from sys import argv
 from os import  system
 
 fileName = argv[1].replace(' ', '_').replace("'", "_") + '.cpp'
+
+cppCodeHeader = '''// #include <bits/stdc++.h>
+#include "/Users/ibaali/bits/stdc++.h"
+
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    // your code here
+}
+'''
+
+with open(fileName, 'w') as file:
+    file.write(cppCodeHeader)
+
 system("/Applications/Visual\ Studio\ Code.app/contents/Resources/app/bin/code " + fileName)
