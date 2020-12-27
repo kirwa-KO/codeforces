b6a4bf70f16cab80f37ae3b64a9fdb6509a4e8dd
root <root@DESKTOP-7O0KF3T.localdomain>
2020-12-28 07:48:13 +0800

    add solution of Toy_Army problem

---
+++ Toy_Army.cpp
@@ -0,0 +1,17 @@
+// #include <bits/stdc++.h>
+#include "stdc++.h"
+
+using namespace std;
+int main()
+{
+    // variabels here
+	int		n;
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    
+    // your code here
+    cin >> n;
+	cout << (n / 2) * 3 << endl;
+	return 0;
+}
