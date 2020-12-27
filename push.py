a4b18166a7099395c091624bb8a80432fcc61590
root <root@DESKTOP-7O0KF3T.localdomain>
2020-12-28 07:49:58 +0800

    add a python code to push problems somutions

---
+++ push.py
@@ -0,0 +1,19 @@
+import subprocess
+
+files = [
+"Coder.cpp",
+"Combination_Lock.cpp",
+"GukiZ_and_Contest.cpp",
+"Levko_and_Table.cpp",
+"Roma_and_Lucky_Numbers.cpp",
+"Soft_Drinking.cpp",
+"Summer_Camp.cpp",
+"Toy_Army.cpp",
+"Wizards__Duel.cpp",
+]
+
+for file in files:
+	cmd = "git add " + file
+	subprocess.call(["git", "add", file])
+	cmd = 'add solution of ' + file.split('.')[0] + ' problem'
+	subprocess.call(["git", "commit", "-m", cmd])
